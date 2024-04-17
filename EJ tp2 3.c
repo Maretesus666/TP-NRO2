f69551360eb476de5bf5426b030660a2d969b9ca
Maretesus666 <166169222+Maretesus666@users.noreply.github.com>
2024-04-17 15:22:57 -0300

    Update and rename EJERCICIO 3.c to EJ tp2 3.c

--- EJ tp2 3.c
+++ EJ tp2 3.c
@@ -1,5 +1,5 @@
 /* 3. Realiza un programa que determine si una cadena de caracteres ingresada
-por el usuario es palíndromo (se lee igual de izquierda a derecha que de
+por el usuario es palÃ­ndromo (se lee igual de izquierda a derecha que de
 derecha a izquierda). */
 
 #include <stdio.h>
